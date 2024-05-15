797bdf68677fe0cd97fc1d8689bf7860bbb32594
Natasha M Oliveira <me@natashamo.dev>
2024-05-14 23:15:20 -0300

    feat: add input component

---
+++ src/components/input.tsx
@@ -0,0 +1,23 @@
+import { Input as BaseInput, InputProps } from '@mui/base/Input'
+import React from 'react'
+
+const Input = React.forwardRef(function Input(
+  { slotProps, ...props }: InputProps,
+  ref: React.ForwardedRef<HTMLInputElement>,
+) {
+  return (
+    <BaseInput
+      {...props}
+      slotProps={{
+        ...slotProps,
+        input: {
+          className:
+            'h-12 w-full text-sm leading-5 px-3 py-2 rounded-md bg-transparent border-2 border-zinc-300 focus:border-indigo-400 outline-none placeholder:text-zinc-500',
+        },
+      }}
+      ref={ref}
+    />
+  )
+})
+
+export default Input
