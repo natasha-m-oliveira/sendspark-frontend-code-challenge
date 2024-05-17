38114804a719164e05345df0d448c63759136231
Natasha M Oliveira <85530160+natasha-m-oliveira@users.noreply.github.com>
2024-05-17 15:45:26 -0300

    fix: update the backend connection url

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'sendspark-backend-code-challenge.vercel.app
+        destination: 'https://sendspark-backend-code-challenge.vercel.app
 /:path*',
       },
     ]
